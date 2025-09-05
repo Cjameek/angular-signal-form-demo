5142bb36bc96d30c81ba97a1a4d3aa1d1009eaaf
Cjameek <cody.meek01@gmail.com>
2025-09-05 19:54:53 -0400

    feat: add submit handling & loading animation

--- src/app/forms/nested-form-controls/employee-form.ts
+++ src/app/forms/nested-form-controls/employee-form.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { form, Control, apply } from '@angular/forms/signals';
+import { form, Control, apply, submit, Field } from '@angular/forms/signals';
 import { User, NestedUserControls } from './user-controls';
 import { Address, NestedAddressControls } from './address-controls';
 import { employeeFormSchema, userSchema, addressSchema } from './schema';
@@ -16,7 +16,12 @@ export interface EmployeeFormState {
   selector: 'nested-employee-form',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-  <form>
+  <aside>
+    <button type="button" (click)="toggleAddress()">Toggle Address Fields</button>
+    <button type="button" (click)="toggleMiddleInitial()">Toggle Middle Initial Field</button>
+  </aside>
+
+  <form (submit)="submitEmployee($event)">
     <fieldset>
       <label for="employeeID">Employee ID # <small>(Required)</small></label>
       <input id="employeeID" type="text" [control]="employeeForm.employeeID" inputmode="numeric" pattern="[0-9]*" />
@@ -32,45 +37,38 @@ export interface EmployeeFormState {
       <input id="requireEmail" type="checkbox" [control]="employeeForm.requireEmail" />
     </div>
 
-    <div class="controls">
+    <section class="controls">
       <nested-user-controls [parentForm]="employeeForm" />
   
       @if(state().address){
         <nested-address-controls [parentForm]="employeeForm" />
       }
-    </div>
+    </section>
 
     @if(employeeForm().errors().length > 0 && employeeForm().touched()){
-      <p>
+      <ul>
         @for(error of employeeForm().errors(); track $index) {
-          <small class="error">{{ error.message }}</small>
+          <li><small class="error">{{ error.message }}</small></li>
         }
-      </p>
+      </ul>
     }
 
-    <div>
-      <button type="button" (click)="toggleAddress()">Toggle Address Fields</button>
-      <button type="button" (click)="toggleMiddleInitial()">Toggle Middle Initial Field</button>
-    </div>
-
-    <pre>
-      Value: {{ employeeForm().value() | json }}
-      Valid: {{ employeeForm().valid() | json }}
-    </pre>
+    <button [disabled]="employeeForm().invalid()">Submit Form</button>
   </form>
+
+  <pre>
+    Value: {{ employeeForm().value() | json }}
+    Valid: {{ employeeForm().valid() | json }}
+  </pre>
+
+  <div class="loading" [attr.aria-hidden]="!employeeForm().submitting()">
+    <strong>Loading...</strong>
+  </div>
   `,
   imports: [Control, JsonPipe, NestedUserControls, NestedAddressControls],
 })
 export class NestedEmployeeForm {
-  readonly state = signal<EmployeeFormState>({
-    employeeID: null,
-    requireEmail: false,
-    user: {
-      firstName: '',
-      lastName: '',
-      email: ''
-    }
-  });
+  readonly state = signal<EmployeeFormState>(this.createNewState());
 
   readonly employeeForm = form(this.state, (profile) => {
     apply(profile, employeeFormSchema),
@@ -120,6 +118,40 @@ export class NestedEmployeeForm {
       } as EmployeeFormState;
       
       this.state.set(updatedState);
+    }
+  }
+
+  submitEmployee(e: SubmitEvent): void {
+    if(this.employeeForm().invalid()) return;
+
+    e.preventDefault();
+
+    // Self submit via submit() fn to trigger the submitting() computed Signal
+    submit(this.employeeForm, async (f) => {
+      return this.submitNewEmployee(f);
+    });
+  }
+
+  async submitNewEmployee(employeeForm: Field<EmployeeFormState, string | number>) {
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Arbitrary loading time
+
+    employeeForm().reset();
+    this.resetFormState(this.createNewState());
+  }
+
+  private resetFormState(newState: EmployeeFormState): void {
+    this.state.set(newState);
+  }
+
+  private createNewState(): EmployeeFormState {
+    return {
+      employeeID: null,
+      requireEmail: false,
+      user: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
     }
   }
 }
